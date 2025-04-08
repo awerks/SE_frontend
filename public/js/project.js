e34753afb6032b6283a4936985c9808b41144423
avalean07 <avalean@constructor.university>
2025-04-08 09:26:59 +0200

    nothing new yet

---
+++ public/js/project.js
@@ -0,0 +1,13 @@
+const API_URL = '/api/projects';
+
+//i guess a helper function for errors as on swagger
+function displayError(error)
+{
+    console.error(error);
+    alert(error);
+}
+//error is the message
+
+
+
+
